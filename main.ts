dbec1156983c9988f7e8b6fe0e9ad64cb722c34b
PiZo68 <67067667+PiZo68@users.noreply.github.com>
2022-04-19 12:31:53 +0200

    vegleges

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+})
